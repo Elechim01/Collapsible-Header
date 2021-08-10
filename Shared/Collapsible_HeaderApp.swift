db00a366b8b01ff713bf7e204bb5cf3604a82327
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-10 22:23:19 +0200

    Initial Commit

---
+++ Shared/Collapsible_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collapsible_HeaderApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 10/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Collapsible_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
